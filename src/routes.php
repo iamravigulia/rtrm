370509e44b80670a107bcca26d6d2fda795d4606
Ravi Gulia <iamravigulia@users.noreply.github.com>
2021-08-17 11:38:04 +0530

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('greeting', function () {
-    return 'Hi, this is your awesome package! Rtrm';
-});
+// Route::get('greeting', function () {
+//     return 'Hi, this is your awesome package! Rtrm';
+// });
 
-Route::get('rtrm/test', 'EdgeWizz\Rtrm\Controllers\RtrmController@test')->name('test');
+// Route::get('rtrm/test', 'EdgeWizz\Rtrm\Controllers\RtrmController@test')->name('test');
 
 Route::post('fmt/rtrm/store', 'EdgeWizz\Rtrm\Controllers\RtrmController@store')->name('fmt.rtrm.store');
